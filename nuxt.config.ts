abc5f36ff13f55bd0767eaa5fda466047fc5c5f3
thierryrudaseswa <trudaseswa@gmail.com>
2024-09-30 20:34:43 +0200

    The additional of metadata ,usehead and dynamic routes in nuxt js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        {name: 'description', content: 'Everything about Nuxt 3'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  },
+
+
   devtools: { enabled: true }
 })
