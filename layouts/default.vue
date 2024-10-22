a8749c207ba900b3d09d60c1b76730cb55940636
thierryrudaseswa <trudaseswa@gmail.com>
2024-10-22 23:00:12 +0200

    the updation of the products file

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
       <header class="shadow-sm bg-white">
         <nav class="container mx-auto p-4 flex justify-between">
           <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
-          <ul class="flex gap-4">
+          <ul class="flex gap-4 p-0">
             <li><NuxtLink to="/">Home</NuxtLink></li>
             <li><NuxtLink to="/about">About</NuxtLink></li>
             <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
