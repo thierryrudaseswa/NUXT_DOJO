9ebea61a52cf3216542abe4845e078714dfe4ee7
thierryrudaseswa <trudaseswa@gmail.com>
2024-11-02 21:40:02 +0200

    The updation of the about page and product

--- pages/about.vue
+++ pages/about.vue
@@ -18,13 +18,7 @@
 </style>
 
 <script setup>
-  // const { data } = await useFetch('/api/ninja?name=mario')
-  // const { data } = await useFetch('/api/ninja?name=mario', {
-  //   method: 'post',
-  //   body: {age: 30}
-  // })
-
-  // const { data } = await useFetch('/api/ninja')
+ 
 
   const { data } = await useFetch('/api/currency/GBP')
 </script>
