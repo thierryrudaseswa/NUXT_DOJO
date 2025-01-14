f7820125c9c61be2948e57a93f685ef467670766
thierryrudaseswa <trudaseswa@gmail.com>
2025-01-14 23:07:26 +0200

    The updation

--- pages/products/error.vue
+++ pages/products/error.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-7 max-w-sm mx-auto text-center card  p-0">
+    <div class="mt-7 max-w-sm mx-auto text-center card ">
       <p class="mt-7 text-7xl font-bold ">{{error.statusCode}}</p>
       <p class="mt-7 text-6xl">Ooops. something </p>
       <p class="mt-7">{{error.message}}</p>
