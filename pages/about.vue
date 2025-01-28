5b9b5437435f1a9d9daee7e0743b568d16e5264f
thierryrudaseswa <trudaseswa@gmail.com>
2025-01-28 21:39:00 +0200

    The updation of about

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="">About</h2>
+    <h2 class=" p-0">About</h2>
     <p>Lorem ipsum dolor  sit amet consectetur adipisicing. Eveniet aperiam minima unde nemo harum quam maxime optio quos corrupti. Eum sapiente facere nemo, laborum ullam non cum fuga quas eveniet harum molestiae minus atque vel ratione illo quia, iure commodi dicta porro excepturi quam. Facere, commodi corrupti ipsam neque totam reprehenderit laboriosam laborum veniam est quo, repellat saepe impedit labore?</p>
     <p>api response here:</p>
     <div>{{ data }}</div>
